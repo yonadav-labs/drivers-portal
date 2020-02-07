f9ac5cfcf1b92f79c309c6dc9e9ce63f50cd9419
dander94 <3balejandro@gmail.com>
2020-02-07 13:54:42 +0100

    STABLE-31: Dashboard structure

---
+++ frontend/src/apps/dashboard/components/navigation/dashboard-menu-item.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="menu-item" :class="{ 'selected': selected }" @click="onClick">
+    <div class="menu-item--line"></div>
+    <div class="menu-item--content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+export default class DashboardMenuItem extends Vue {
+  @Prop({ default: false })
+  selected!: boolean
+
+  onClick(): void {
+    this.$emit('click');
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .svg-icon-center {
+      display: flex;
+  }
+</style>
+
+<style lang="scss" scoped>
+.menu-item {
+  display: flex;
+  align-items: stretch;
+  justify-content: space-between;
+  cursor: pointer;
+  width: 46px;
+  margin: 10px 10px 10px 0px;
+
+  .menu-item--line {
+    visibility: hidden;
+    border-radius: 5px;
+    border: 2px solid $blue;
+  }
+
+  .menu-item--content {
+    align-items: center;
+    border-radius: 2px;
+    display: flex;
+    height: 36px;
+    justify-content: center;
+    width: 36px;
+
+    &:hover {
+      background-color: rgba($grey, 0.2);
+    }
+
+    div {
+      color: $blue;
+    }
+  }
+
+  &.selected {
+    .menu-item--line {
+      visibility: visible;
+    }
+
+    .menu-item--content {
+      background-color: rgba($grey, 0.2);
+    }
+  }
+}
+</style>
---
+++ frontend/src/apps/dashboard/views/main.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="dashboard-view">
+    <div class="dashboard-view-container">
+      <dashboard-navbar></dashboard-navbar>
+      <div class="dashboard-container">
+        <dashboard-menu></dashboard-menu>
+        <div class="dashboard-content">
+          <router-view></router-view>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+import DashboardNavbar from '@/apps/dashboard/components/navigation/dashboard-navbar.vue'
+import DashboardMenu from '@/apps/dashboard/components/navigation/dashboard-menu.vue'
+
+import { DashboardRouteName } from '@/router/dashboard'
+
+@Component({
+  components: {
+    DashboardNavbar, DashboardMenu
+  }
+})
+export default class Dashboard extends Vue {
+}
+</script>
+
+<style lang="scss" scoped>
+#dashboard-view {
+  background-color: $grey-light;
+  .dashboard-view-container{
+    background-color: $white;
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    margin: 0 auto;
+    max-width: 73.13rem;
+    padding: 4px 10px;
+    width: 100%;
+  }
+  .dashboard-container {
+    align-items: stretch;
+    display: flex;
+    height: 100%;
+
+    .dashboard-content {
+      padding: 1.875rem 1.25rem 1.875rem 1.875rem;
+      width: 100%;
+    }
+  }
+}
+</style>
