89419231e244792c0c5021013a00aad0d5c6123d
dander94 <3balejandro@gmail.com>
2020-02-08 12:15:36 +0100

    STABLE-33: Retrieve user and set password api methods

--- frontend/src/store/users/api.ts
+++ frontend/src/store/users/api.ts
@@ -1,6 +1,18 @@
 import { client } from '@/store/api'
 
+import { User } from '@/@types/users';
+
 export async function checkEmailExists(email: string): Promise<{ email: string }> {
-  const response = await client.get(`users/${email}/exists`)
+  const response = await client.get(`users/${email}/exists/`)
+  return response.data
+}
+
+export async function getCurrentUser(): Promise<User> {
+  const response = await client.get(`users/current/`)
+  return response.data
+}
+
+export async function updateUserPassword(password: string): Promise<User> {
+  const response = await client.put(`users/current/set_password/`, { password })
   return response.data
 }
