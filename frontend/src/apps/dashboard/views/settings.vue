fb9b9aa57a00b1aa1cfc94cd38e51a396f302d00
danibacien <dani@commite.co>
2020-03-19 13:37:09 +0100

    feat(settings): Add change password styles and functionality

--- frontend/src/apps/dashboard/views/settings.vue
+++ frontend/src/apps/dashboard/views/settings.vue
@@ -33,12 +33,13 @@
           </div>
           <p class="error"><error-message v-if="passwordLengthValid && this.confirmPassword.length >= this.password.length && !passwordsMatch">The passwords must match</error-message></p>
           <div class="settings-box__action">
-            <basic-button
+            <contained-button
               :disabled="!passwordValid"
-              text='Create password'
-              :color='colorBlue'
-              >
-            </basic-button>   
+              color="blue"
+              @click="changePassword"
+            >
+              Change password
+            </contained-button>
           </div>
         </div>
       </form>
@@ -83,7 +84,7 @@ import { Component, Vue, Prop } from 'vue-property-decorator';
 import { Getter, Action, namespace } from 'vuex-class';
 
 import BasicInput from '@/components/inputs/basic-input.vue'
-import BasicButton from '@/components/buttons/basic-button.vue'
+import ContainedButton from '@/components/buttons/contained-button.vue'
 import ErrorMessage from '@/components/error-message.vue'
 
 
@@ -92,14 +93,19 @@ import { RouteName } from '@/router';
 
 import { Colors } from '@/utils/colors'
 
+const users = namespace('Users')
+
 @Component({
   components: {
-    BasicInput, BasicButton, ErrorMessage
+    BasicInput, ContainedButton, ErrorMessage
   }
 })
 export default class DashboardSettingsView extends Vue {
   confirmPassword = ''
   password = ''
+
+  @users.Action
+  updateUserPassword!: (password: string) => Promise<void>
 
   get colorBlue(): string {
     return Colors.Blue
@@ -117,9 +123,69 @@ export default class DashboardSettingsView extends Vue {
     return this.passwordLengthValid && this.passwordsMatch;
   }
 
+  async changePassword(): Promise<void> {
+    await this.updateUserPassword(this.password)
+    this.password = ''
+    this.confirmPassword = ''
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+  .title {
+    font-size: 1.25rem;
+    font-weight: $fw-semibold;
+    line-height: 24px;
+    margin-bottom: 1rem;
+  }
 
+  .settings-box {
+    display: flex;
+    flex-direction: column;
+    max-width: 18rem;
+    
+    .settings-box__title {
+      font-size: $fs-lg;
+      line-height: 20px;
+      margin-bottom: 1rem;
+    }
+
+    .form-input {
+      border-radius: 8px;
+      margin-bottom: 1.25rem;
+      text-align: left;
+
+      .form-input__container {
+        padding: 1.25rem 2.5rem;
+      }
+
+      .form-input__field {
+        align-items: flex-start;
+        display: flex;
+        flex-direction: column;
+        justify-content:flex-start;
+
+        &:not(:first-child) {
+          margin-top: 1.25rem;
+        }
+      }
+
+      .form-input__label {
+        font-size: $fs-md;
+        font-weight: $fw-semibold;
+        margin-bottom: 0.5rem;
+        opacity: 0.8;
+      }
+
+      .form-input__input {
+        width: 17.5rem;
+      }
+    }
+
+    .settings-box__action {
+      display: flex;
+      justify-content: center;
+      margin-top: 1rem;
+    }
+  }
 </style>
